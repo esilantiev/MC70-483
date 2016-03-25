165146dc5f719e177e198cc727b38730b52bdff0
esilantiev <silantiev.evgheni@gmail.com>
2016-03-25 16:02:57 +0200

    Modified

--- MicrosoftCertification70-483/MultiThreading/Program.cs
+++ MicrosoftCertification70-483/MultiThreading/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultiThreading
 {
-    class Program
+    public class Program
     {
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        } 
         static void Main(string[] args)
         {
+            bool stopped = false;
+
+            Thread t = new Thread(new ThreadStart(()=>ThreadMethod(5)));
+
+            t.IsBackground = true;
+            t.Start();          
+            
+            t.Join();
         }
     }
 }
